0afc6b338a8f4a41d51e787163386b9c898f4534
switchingaura17 <71724588+switchingaura17@users.noreply.github.com>
2020-09-29 09:16:04 -0600

    started bouncing circles and fixed the template master

--- bouncing circles/cs30-p5js-template-master/sketch.js
+++ bouncing circles/cs30-p5js-template-master/sketch.js
@@ -5,6 +5,7 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let shouldIShowTheEllipse = false;
 
 
 function setup() {
@@ -13,4 +14,15 @@ function setup() {
 
 function draw() {
   background(220);
+
+  let something = true;
+  
+  if (shouldIShowTheEllipse) {
+    fill("red");
+    ellipse(mouseX, mouseY, 250, 250);
+  }
+}
+
+function mousePressed() {
+  shouldIShowTheEllipse = true;
 }
