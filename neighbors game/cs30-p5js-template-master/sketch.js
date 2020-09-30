0f0c4d38f2f77b10dc4ad5ae5b9f61e6e42e392e
switchingaura17 <71724588+switchingaura17@users.noreply.github.com>
2020-09-30 10:30:57 -0600

    cool game

---
+++ neighbors game/cs30-p5js-template-master/sketch.js
@@ -0,0 +1,90 @@
+let grid;
+let cellWidth;
+let cellHeight;
+const GRIDSIZE = 4
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  grid = generateEmptyGrid(GRIDSIZE);
+  cellWidth = width / grid[0].length;
+  cellHeight = height / grid.length;
+}
+
+function draw() {
+  background(220);
+  displayGrid();
+}
+
+function keyPressed() {
+  if (key === " ") {
+    grid = generateRandomGrid(10);
+  }
+}
+
+function displayGrid() {
+  
+
+  for (let y=0; y<grid.length; y++) {
+    for (let x=0; x<grid[y].length; x++) {
+      if (grid[y][x] === 0) {
+        fill("white");
+      }
+      else {
+        fill("black");
+      }
+
+      rect(cellWidth*x, cellHeight*y, cellWidth, cellHeight);
+    }
+  }
+}
+
+
+function generateRandomGrid(gridSize) {
+  let grid = [];
+  for (let i=0; i<gridSize; i++) {
+    grid.push([]);
+    for (let j=0; j<gridSize; j++) {
+      if (random(100) < 50) {
+        grid[i].push(0);
+      }
+      else {
+        grid[i].push(1);
+      }
+    }
+  }
+  return grid;
+}
+
+function mousePressed() {
+  let cellX = floor(mouseX / cellWidth);
+  let cellY = floor(mouseY / cellHeight);
+
+  toggleCell(cellX, cellY);
+  toggleCell(cellX, cellY - 1);
+  toggleCell(cellX, cellY + 1);
+  toggleCell(cellX + 1, cellY);
+  toggleCell(cellX - 1, cellY);
+}
+
+function generateEmptyGrid(gridSize) {
+  let grid = [];
+  for (let i=0; i<gridSize; i++) {
+    grid.push([]);
+    for (let j=0; j<gridSize; j++) {
+      grid[i].push(0);
+    }
+    
+  }
+  return grid;
+}
+
+function toggleCell(cellX, cellY) {
+  if (cellX >= 0 && cellX < GRIDSIZE && cellY >= 0 && cellY < GRIDSIZE) {
+    if (grid[cellY][cellX] === 0) {
+      grid[cellY][cellX] = 1;
+    }
+    else {
+      grid[cellY][cellX] = 0;
+    }
+  }
+}
