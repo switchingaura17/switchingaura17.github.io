42731d491edca867e50b007e3885ffeeb2b0b58d
switchingaura17 <71724588+switchingaura17@users.noreply.github.com>
2020-10-02 09:52:03 -0600

    sudoku demo cont.

--- soduko/sketch.js
+++ soduko/sketch.js
@@ -4,30 +4,29 @@
 
 const GRIDSIZE = 9;
 let cellSize;
+let sudoku;
+let initialState;
 
-
-let sudoku = [[5,3,0,0,7,0,0,0,0], 
-              [6,0,0,1,9,5,0,0,0], 
-              [0,9,8,0,0,0,0,6,0], 
-              [8,0,0,0,6,0,0,0,3], 
-              [4,0,0,8,0,3,0,0,1], 
-              [7,0,0,0,2,0,0,0,6],
-              [0,6,0,0,0,0,2,8,0], 
-              [0,0,0,4,1,9,0,0,5], 
-              [0,0,0,0,8,0,0,7,9]];
-
-let initialState = [[5,3,0,0,7,0,0,0,0], 
-              [6,0,0,1,9,5,0,0,0], 
-              [0,9,8,0,0,0,0,6,0], 
-              [8,0,0,0,6,0,0,0,3], 
-              [4,0,0,8,0,3,0,0,1], 
-              [7,0,0,0,2,0,0,0,6],
-              [0,6,0,0,0,0,2,8,0], 
-              [0,0,0,4,1,9,0,0,5], 
-              [0,0,0,0,8,0,0,7,9]];
+function preload() {
+  sudoku = loadStrings("assets/2.txt");
+  initialState = loadStrings("assets/2.txt");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  for (let i = 0; i < sudoku.length; i++) {
+    sudoku[i] = sudoku[i].split(",");
+    initialState[i] = initialState[i].split(",");
+  }
+
+  for (let y = 0; y < GRIDSIZE; y++) {
+    for (let x = 0 ; x < GRIDSIZE; x++) {
+      sudoku[y][x] = int(sudoku[y][x]);
+      initialState[y][x] = int(initialState[y][x]);
+    }
+  }
+
   if (width < height) {
     cellSize = width / GRIDSIZE;
   }
@@ -55,7 +54,7 @@ function displaygrid() {
         else {
           fill("black");
         }
-        textSize((cellSize * 0.8));
+        textSize(cellSize * 0.8);
         textAlign(CENTER, CENTER);
         text(sudoku[y][x], x * cellSize + cellSize / 2, y * cellSize + cellSize / 2);
       }
